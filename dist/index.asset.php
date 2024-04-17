4af25fc6248d29128998d2890fec11ee516362ee
Monir <fencemonir@gmail.com>
2024-04-17 14:54:45 +0600

    update modules

--- dist/index.asset.php
+++ dist/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '4f6072a06c772f481bd3');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'be9a3b7363e59ec8ae1e');
